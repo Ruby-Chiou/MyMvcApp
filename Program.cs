eec0e4560248aaf781ee67e2c3a1045613feb748
Ruby-Chiou <12130906@me.mcu.edu.tw>
2025-04-17 20:16:44 +0800

    移除新增商品畫面的 ID 輸入欄位

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using MyMvcApp.Data;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load();
+String connectionString=Environment.GetEnvironmentVariable("DB_URL") ?? throw new Exception("DB_URL is not set.");
 
 // 註冊 Controller 與 View
 builder.Services.AddControllersWithViews();
 
 // 註冊 ApplicationDbContext 並使用 PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseNpgsql(connectionString)
 );
 
 var app = builder.Build();
